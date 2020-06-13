0a1aa54f5a917d433f9fbca17b15eea36797be85
Rosario Arguello <mrosario.arguello@gmail.com>
2020-06-13 15:03:50 +0200

    Add initial questionDetail page implementation

--- src/pages/QuestionDetail.tsx
+++ src/pages/QuestionDetail.tsx
@@ -1,14 +1,46 @@
-import React from 'react';
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Question } from '../types';
+import { useParams, Redirect } from "react-router-dom";
+import { useStateValue } from '../context/context';
 
 const QuestionDetail = () => {
   const { id } = useParams();
+  const [currentQuestion, setCurrentQuestion] = useState<Question | undefined>();
+  const [context, dispatch] = useStateValue();
+  const { loading, error, questions } = context;
 
+  useEffect(() => {
+    if (loading) {
+      dispatch({ type: 'toggleLoading' });
+    }
+    if (questions) {
+      setCurrentQuestion(questions.find((question: Question) => question.url === `/questions/${id}`))
+    }
+  }, [currentQuestion, loading, context, id, questions, dispatch])
+
+  if (!questions) {
+    return <Redirect to='/' />
+  }
   return (
-    <div className="container is-widescreen">
-      <h1 className="title is-2">Question: {id} </h1>
-      Options go here
-    </div>)
+    <>
+      {loading && <span data-testid="loading-element">loading...</span>}
+
+      {!loading && error ? <span data-testid="error-element">{error}</span> : questions && (
+        <div className="container is-widescreen">
+          {currentQuestion && (
+            <>
+              <h1 className="title is-3">Question: {currentQuestion.question}</h1>
+              {currentQuestion.choices.map((choice) =>
+                <div key={choice.choice}>
+                  <p>{choice.choice}</p>
+                  <p>{choice.votes}</p>
+                </div>
+              )}
+            </>)}
+        </div>
+      )}
+    </>
+  )
 }
 
 export default QuestionDetail;
--- src/pages/Questions.tsx
+++ src/pages/Questions.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { Question, getQuestionsCallbackType } from '../types';
 import QuestionSummary from '../components/QuestionSummary';
 import { useStateValue } from '../context/context';
@@ -8,27 +8,29 @@ const Questions = () => {
   const [context, dispatch] = useStateValue();
   const { loading, error, questions } = context;
 
-  const getQuestionsCallback: getQuestionsCallbackType = (questions, error) => {
-    dispatch({ type: 'toggleLoading' });
-
+  const getQuestionsCallback: getQuestionsCallbackType = useCallback((questions, error) => {
+    if (loading) {
+      dispatch({ type: 'toggleLoading' });
+    }
     if (error) {
       return dispatch({ type: 'setError', payload: error });
     }
 
     dispatch({ type: 'setQuestions', payload: questions });
-  }
+  }, [dispatch, loading])
+
   useEffect(() => {
     if (!questions || questions.length < 1) {
       GetQuestions(getQuestionsCallback);
     } else if (loading) {
       dispatch({ type: 'toggleLoading' });
     }
-  }, []);
+  }, [loading, context, questions, dispatch, getQuestionsCallback]);
 
   return (
     <div className="container is-widescreen">
       {loading && <span data-testid="loading-element">loading...</span>}
-      {!loading && <h1 className="title is-2">All Questions</h1>}
+      {!loading && <h1 className="title is-3">All Questions</h1>}
       {!loading && error ? <span data-testid="error-element">{error}</span> : questions && (
         <div className="question-container">
           {questions.map((currentQuestion: Question) => (
